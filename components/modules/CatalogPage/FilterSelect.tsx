cf1c4a6e40eab45d5ee7aac24fb2899b51f57638
Esin <kubanesin@gmail.com>
2023-09-29 09:22:15 +0300

    fix: применил effector для сортировки по цене и популярности

--- components/modules/CatalogPage/FilterSelect.tsx
+++ components/modules/CatalogPage/FilterSelect.tsx
@@ -1,24 +1,24 @@
 /* eslint-disable indent */
 import React from 'react'
-import { useDispatch } from 'react-redux'
 import Select from 'react-select'
 import { useStore } from 'effector-react'
-
+import { useRouter } from 'next/router'
 import { IOption, SelectOptionType } from '@/types/common'
 import { createSelectOption } from '@/utils/common'
 import { controlStyles, menuStyles, selectStyles } from './select'
 import { optionStyles } from '@/components/elements/Header/SearchInput'
 import { categoriesOptions } from '@/utils/selectContents'
+import { $mode } from '@/context/mode'
 import {
+  $boilerParts,
   setBoilerPartsByPopularity,
   setBoilerPartsCheapFirst,
   setBoilerPartsExpensiveFirst,
-} from '@/redux/slices/boilerParts'
-import { $mode } from '@/context/mode'
+} from '@/context/boilerParts'
 
 const FilterSelect = () => {
   const mode = useStore($mode)
-
+  const router = useRouter()
   // состояние выподающего списка инпута-поиска
   const [categoryOption, setCategoryOption] =
     React.useState<SelectOptionType>(null)
@@ -26,20 +26,63 @@ const FilterSelect = () => {
   // const [options, setOptions] = React.useState(
   //   [1, 2, 3, 4, 5].map((item) => ({ value: item, label: item }))
   // )
-  const dispatch = useDispatch()
+  const boilerParts = useStore($boilerParts)
 
-  // функция которя будет сетить каждую опцию
+  // делаю так чтобы при перезагрузке сохранялся путь в адресной строке
+  React.useEffect(() => {
+    if (boilerParts.rows) {
+      switch (router.query.first) {
+        case 'cheap':
+          updateCategoryOption('Сначала дешевые')
+          setBoilerPartsCheapFirst()
+          break
+        case 'expensive':
+          updateCategoryOption('Сначала дорогие')
+          setBoilerPartsExpensiveFirst()
+          break
+        case 'popular':
+          updateCategoryOption('По популярности')
+          setBoilerPartsByPopularity()
+          break
+        default:
+          updateCategoryOption('Сначала дешевые')
+          setBoilerPartsCheapFirst()
+          break
+      }
+    }
+  }, [boilerParts.rows, router.query.first])
+
+  const updateCategoryOption = (value: string) =>
+    setCategoryOption({ value, label: value })
+
+  // функция поторая позволит сохранить выбранную сортировку товаров даже после обновления страницы
+  const updateRoteParam = (first: string) =>
+    router.push(
+      {
+        query: {
+          ...router.query,
+          first,
+        },
+      },
+      undefined,
+      { shallow: true }
+    )
+
+  // функция которая будет сетить каждую опцию
   const handleSortOptionChange = (selectedOption: SelectOptionType) => {
     setCategoryOption(selectedOption)
     switch ((selectedOption as IOption).value) {
       case 'Сначала дешевые':
-        dispatch(setBoilerPartsCheapFirst(selectedOption))
+        setBoilerPartsCheapFirst()
+        updateRoteParam('cheap')
         break
       case 'Сначала дорогие':
-        dispatch(setBoilerPartsExpensiveFirst(selectedOption))
+        setBoilerPartsExpensiveFirst()
+        updateRoteParam('expensive')
         break
       case 'По популярности':
-        dispatch(setBoilerPartsByPopularity(selectedOption))
+        setBoilerPartsByPopularity()
+        updateRoteParam('popular')
         break
     }
   }
